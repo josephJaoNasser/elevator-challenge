fe3c57673273d14d1d392289f03a9088ca092684
Joseph Nasser <57519561+josephJaoNasser@users.noreply.github.com>
2022-01-10 20:09:43 +0800

    Added inputs for adding new riders/requests

--- elevator-challenge-web-app/client/js/Elevator.js
+++ elevator-challenge-web-app/client/js/Elevator.js
@@ -1,3 +1,23 @@
+/*
+Call these events in your function to trigger the animation on the front end.
+THE ANIMATIONS WILL NOT PLAY IF YOU DO NOT CALL THE EVENTS
+example: 
+{
+  ... your code ...
+
+  this.callEvent('event_Name', parameters) or this.callEvent('event_Name')
+}
+
+Events and what they do:
+'elevatorDispatched' = call when user clicks 'Dispatch elevator'
+'done' = called when elevator squence is done
+'floorChanged' = call when elevator floor is changed.
+'stop' = call when elevator stops/should stop on a floor
+'riderEntered' = call when a rider has entered. Requires an array of Person
+'riderExit' = call when a rider leaves the elevator. Requires an array of Person
+
+*/
+
 class Elevator {
   constructor() {
     this.currentFloor = 0
--- elevator-challenge-web-app/client/js/animate.js
+++ elevator-challenge-web-app/client/js/animate.js
@@ -1,114 +1,169 @@
-const dispatchButton = document.getElementById('btn-dispatch')
-const ridersList = document.getElementById('ol-riders')
-const requestsList = document.getElementById('ul-requests')
-const elevatorRender = document.getElementById('elevator-container')
-const floorDisplay = document.getElementById('floor-display')
-const actionIndicator = document.getElementById('current-action')
-const elevator = new Elevator()    
+const dispatchButton = document.getElementById("btn-dispatch");
+const addRiderButton = document.getElementById("button-add-rider");
+const riderNameInput = document.getElementsByName("name");
+const riderStartingFloorInput = document.getElementsByName("startingFloor");
+const riderDropoffFloorInput = document.getElementsByName("dropoffFloor");
+const ridersList = document.getElementById("ol-riders");
+const requestsList = document.getElementById("ul-requests");
+const elevatorRender = document.getElementById("elevator-container");
+const elevatorRail = document.getElementsByClassName("elevator-rail")
+const floorDisplay = document.getElementById("floor-display");
+const actionIndicator = document.getElementById("current-action");
+const elevator = new Elevator();
 
-let sequence = []
+let sequence = [];
+elevator.requests = [
+  new Person("Bill", 3, 0),
+  new Person("Kay", 2, 5),
+  new Person("Jack", 3, 0),
+];
 
-dispatchButton.addEventListener('click', ()=>{
-  elevator.requests = [new Person('Bill', 3, 0), new Person('Kay',2,5), new Person('Jack', 3, 0)]
-  actionIndicator.textContent=''
-  listRequests()
+listRequests();
 
-  elevator.dispatch()  
-})
+// EVENT LISTENERS
 
-elevator.on('floorChanged', ()=> {
-  sequence.push({ event: 'floorChanged', floor: elevator.currentFloor})
-})
+addRiderButton.addEventListener("click", () => {
+  elevator.requests.push(
+    new Person(
+      riderNameInput[0].value,
+      +riderStartingFloorInput[0].value,
+      +riderDropoffFloorInput[0].value
+    )
+  );
+  listRequests();
+});
 
-elevator.on('riderEntered', pickups => {
-  pickups.length && sequence.push({ event: 'riderEntered', pickups: pickups})  
-})
+dispatchButton.addEventListener("click", () => {
+  //elevator.requests = riders;
+  actionIndicator.textContent = "";
+  listRequests();
 
-elevator.on('riderExit', dropOffs => {
-  dropOffs.length && sequence.push({ event: 'riderExit', dropOffs: dropOffs})
-})
+  elevator.dispatch();
+});
 
+elevator.on("floorChanged", () => {
+  sequence.push({ event: "floorChanged", floor: elevator.currentFloor });
+});
 
-elevator.on('done', () => {
-  playAnimation()
-})
+elevator.on("riderEntered", (pickups) => {
+  pickups.length && sequence.push({ event: "riderEntered", pickups: pickups });
+});
 
-function playAnimation(){
+elevator.on("riderExit", (dropOffs) => {
+  dropOffs.length && sequence.push({ event: "riderExit", dropOffs: dropOffs });
+});
+
+elevator.on("done", () => {
+  playAnimation();
+});
+
+//FUNCTIONS
+function playAnimation() {
   let i = 0;
 
-  const interval = setInterval(()=> {    
-    if(i > sequence.length -1){
-      clearInterval(interval)
-      sequence=[]
+  const interval = setInterval(() => {
+    if (i > sequence.length - 1) {
+      clearInterval(interval);
+      sequence = [];
     }
-    switch (sequence[i].event){  
-      case 'floorChanged': 
-        moveElevator(sequence[i].floor)
-        i++
-        return
-      case 'riderEntered':
-        listRiders(sequence[i].pickups)
-        i++
-        return
-      case 'riderExit':
-        removeRiders(sequence[i].dropOffs[0].dropOffFloor)
-        i++
-        return
+    switch (sequence[i].event) {
+      case "floorChanged":
+        moveElevator(sequence[i].floor);
+        i++;
+        return;
+      case "riderEntered":
+        listRiders(sequence[i].pickups);
+        i++;
+        return;
+      case "riderExit":
+        removeRiders(sequence[i].dropOffs[0].dropOffFloor);
+        i++;
+        return;
       default:
-        return
+        return;
     }
-  },1000)
+  }, 1000);
+}
+
+function getHighestFloor() {
+  const highestDropoff = Math.max.apply(Math, elevator.requests.map((r)=> r.dropOffFloor))
+  const highestStarting = Math.max.apply(Math, elevator.requests.map((r)=> r.currentFloor))
+  const highestFloor = highestDropoff > highestStarting ? highestDropoff : highestStarting
+
+  elevatorRail[0].style.height = `calc(210px * ${highestFloor + 1})`
+}
+
+function listRequests() {
+  requestsList.replaceChildren([])
+  elevator.requests.forEach((request) => {
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(
+        `${request.name} , puckup @ floor: ${request.currentFloor} Drop-off floor:  ${request.dropOffFloor}`
+      )
+    );
+    li.classList.add("pickupFloor-" + request.currentFloor);
+    requestsList.append(li);
+  });
   
+  getHighestFloor()
 }
 
-function listRequests(){
-  elevator.requests.forEach(request => {
-    const li = document.createElement('li')
-    li.appendChild(document.createTextNode(`${request.name} , puckup @ floor: ${request.currentFloor} Drop-off floor:  ${request.dropOffFloor}`))
-    li.classList.add('pickupFloor-'+request.currentFloor)
-    requestsList.append(li)
-  });
+function moveElevator(to) {
+  elevatorRender.animate(
+    [
+      {
+        transform: `translateY(${to * 100}%)`,
+      },
+    ],
+    {
+      duration: 1000,
+    }
+  ).onfinish = (e) => {
+    floorDisplay.firstChild.replaceWith(to);
+    elevatorRender.style.transform = `translateY(${to * 100}%)`;
+  };
+  return true;
 }
 
-function moveElevator(to){
-  elevatorRender.animate([{
-    transform: `translateY(${to*100}%)`,
-  }], {
-    duration: 1000
-  }).onfinish = (e) => {
-    floorDisplay.firstChild.replaceWith(to)
-    elevatorRender.style.transform = `translateY(${to*100}%)`
-  }
-  return true
-}
-
-function listRiders(riders){
-  const pickupPassengers = document.querySelectorAll('.pickupFloor-'+riders[0].currentFloor)
-  pickupPassengers.forEach(passenger => {
-    passenger.parentNode.removeChild(passenger)
-  })
-
-  let action = 'Picked up '
-
-  riders.forEach(rider => {
-    const li = document.createElement('li')
-    action += rider.name + ', '
-    li.appendChild(document.createTextNode(rider.name +', Drop-off floor: ' + rider.dropOffFloor))
-    li.classList.add('dropOffFloor-'+rider.dropOffFloor)
-    ridersList.append(li)
+function listRiders(riders) {
+  const pickupPassengers = document.querySelectorAll(
+    ".pickupFloor-" + riders[0].currentFloor
+  );
+  pickupPassengers.forEach((passenger) => {
+    passenger.parentNode.removeChild(passenger);
   });
 
-  actionIndicator.textContent=action
+  let action = "Picked up ";
+
+  riders.forEach((rider) => {
+    const li = document.createElement("li");
+    action += rider.name + ", ";
+    li.appendChild(
+      document.createTextNode(
+        rider.name + ", Drop-off floor: " + rider.dropOffFloor
+      )
+    );
+    li.classList.add("dropOffFloor-" + rider.dropOffFloor);
+    ridersList.append(li);
+  });
+
+  actionIndicator.textContent = action;
 }
 
-function removeRiders(dropOffFloor){
-  let action = 'Dropped off '
-  const dropOffPassengers = document.querySelectorAll('.dropOffFloor-'+dropOffFloor)
-  dropOffPassengers.forEach(passenger => {
-    const passengerName = passenger.innerHTML.substr(0, passenger.innerHTML.indexOf(','))
-    action += passengerName + ', '
-    passenger.parentNode.removeChild(passenger)
-  })
+function removeRiders(dropOffFloor) {
+  let action = "Dropped off ";
+  const dropOffPassengers = document.querySelectorAll(
+    ".dropOffFloor-" + dropOffFloor
+  );
+  dropOffPassengers.forEach((passenger) => {
+    const passengerName = passenger.innerHTML.substr(
+      0,
+      passenger.innerHTML.indexOf(",")
+    );
+    action += passengerName + ", ";
+    passenger.parentNode.removeChild(passenger);
+  });
 
-  actionIndicator.textContent=action
+  actionIndicator.textContent = action;
 }
