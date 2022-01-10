01862281ba46e7d9c268307a3932eeea4b02c30a
Joseph Nasser <57519561+josephJaoNasser@users.noreply.github.com>
2022-01-10 20:13:06 +0800

    renamed files

--- elevator-challenge-web-app/client/js/Elevator.js
+++ elevator-challenge-web-app/client/js/Elevator.js
@@ -20,11 +20,6 @@ Events and what they do:
 
 class Elevator {
   constructor() {
-    this.currentFloor = 0
-    this.stops = 0
-    this.floorsTraversed = 0
-    this.requests = []
-    this.riders = []
     this._events = []
   }
 
@@ -44,107 +39,8 @@ class Elevator {
     this._events[name][0](params)    
   }
 
-  dispatch(){
-    this.callEvent('elevatorDispatched')
-    this.requests.forEach(request => {
-      if(this.riders.length || this.requests.length){
-        this.goToFloor(request)
-      }
-    })
-    this.callEvent('done')
-  }
+  //add all your functions below
+  // ...
 
-  goToFloor(person){  
-    //pickup person
-    while(this.currentFloor !== person.currentFloor){
-      if(person.currentFloor > this.currentFloor){
-        this.moveUp()
-      }
-      else{
-        this.moveDown()
-      }
-    }
-
-    //drop off people
-    this.riders.forEach(rider => {
-      while(this.currentFloor !== rider.dropOffFloor && this.riders.length > 0){
-        if(rider.dropOffFloor > this.currentFloor){
-          this.moveUp()
-        }
-        else{
-          this.moveDown()
-        }
-      }
-    })
-
-    //check if the elevator should return to the loby
-    this.checkReturnToLoby() && this.returnToLoby()
-  }
-
-  moveUp(){
-    this.currentFloor++
-    this.callEvent('floorChanged')
-    this.floorsTraversed++
-    if(this.hasStop()){
-      this.stops++
-    }   
-  }
-
-  moveDown(){
-    if(this.currentFloor > 0){    
-      this.currentFloor--
-      this.callEvent('floorChanged')
-      this.floorsTraversed++
-      if(this.hasStop()){
-        this.callEvent('stop')
-        this.stops++
-      }
-    }
-  }
-
-  hasStop(){
-    return (this.hasPickup() || this.hasDropoff()) && (this.floorsTraversed > 0) && this.callEvent('stop')
-  }
-
-  hasPickup(){
-    const pickups = this.requests.filter(request => request.currentFloor === this.currentFloor)
-    const hasPickup = pickups.length ? true : false
-
-    this.requests = this.requests.filter(request => request.currentFloor !== this.currentFloor)
-    pickups.forEach(pickup => this.riders.push(pickup))
-
-    this.callEvent('riderEntered', pickups)
-    return hasPickup
-  }
-
-  hasDropoff(){
-    const dropOffs = this.riders.filter(rider => rider.dropOffFloor === this.currentFloor)
-    const hasDropoff = dropOffs.length ? true : false
-
-    this.riders = this.riders.filter(rider => rider.dropOffFloor !== this.currentFloor)
-
-    this.callEvent('riderExit', dropOffs)
-    return hasDropoff
-  }
-
-  checkReturnToLoby(){
-    if(new Date().getHours() < 12 && !this.riders.length){  
-      return true
-    }
-
-    return false
-  }
-
-  returnToLoby(){
-    while(this.currentFloor > 0){
-      this.moveDown()
-    }
-  }
-
-  reset(){
-    this.currentFloor = 0
-    this.stops = 0
-    this.floorsTraversed = 0
-    this.riders = []
-  }
+  
 }
